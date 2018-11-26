03b332afdfbfd1b8705341b056deceedea657b40
marnokulmala <r3kuma00@students.oamk.fi>
2018-11-26 19:27:23 +0200

    Adding missing query for sending email

--- src/main/kotlin/org/mmkulmala/cvbank/graphql/Query.kt
+++ src/main/kotlin/org/mmkulmala/cvbank/graphql/Query.kt
@@ -1,9 +1,15 @@
 package org.mmkulmala.cvbank.graphql
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver
+import org.mmkulmala.cvbank.services.EmailService
 import org.springframework.stereotype.Component
 
 @Component
-class Query : GraphQLQueryResolver {
+class Query(
+        private val emailService: EmailService
+) : GraphQLQueryResolver {
     fun version() = "1.0.0"
+
+    fun sendAsEmail(senderEmail : String, password : String, toMail : String, resumeName: String)
+            = emailService.sendCVAsAttachment(senderEmail, password, toMail, resumeName)
 }
