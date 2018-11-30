c96ab337935355c094ebcc973d537ab96ea09828
marnokulmala <r3kuma00@students.oamk.fi>
2018-11-30 21:23:24 +0200

    fixed delete method

--- src/main/kotlin/org/mmkulmala/cvbank/dao/CurriculumVitaeDao.kt
+++ src/main/kotlin/org/mmkulmala/cvbank/dao/CurriculumVitaeDao.kt
@@ -16,7 +16,7 @@ class CurriculumVitaeDao(
         private val curriculumVitaeRepository: CurriculumVitaeRepository
 ) {
     // using finnish time format
-    private fun String.toDate() : Date {
+    private fun String.toDate(): Date {
         return SimpleDateFormat("dd.MM.yyyy").parse(this)
     }
 
@@ -50,7 +50,10 @@ class CurriculumVitaeDao(
                     social = social, employment = employment, education = education, skills = skills, samples = samples,
                     references = references, languages = languages, interests = interests))
 
-    fun deleteCurriculumVitaeById(id: String) = curriculumVitaeRepository.deleteById(id)
+    fun deleteCurriculumVitaeById(id: String): String {
+        curriculumVitaeRepository.deleteById(id)
+        return id
+    }
 
     fun updateCurriculumVitae(id: String, updatedCV: CurriculumVitaeInput) = curriculumVitaeRepository.save(
             CurriculumVitae(id = id, name = updatedCV.name,
@@ -61,18 +64,25 @@ class CurriculumVitaeDao(
                             other = updatedCV.contact.other.map { Other(label = it.label, flavor = it.label, value = it.value) }),
                     location = Location(address = updatedCV.location.address, city = updatedCV.location.city, region = updatedCV.location.region,
                             code = updatedCV.location.code, countryCode = updatedCV.location.countryCode),
-                    projects = updatedCV.projects.map { Project(title = it.title, category = it.category, role = it.role, url = it.url, start = it.start, end = it.end,
-                            repo = it.repo, description = it.description, summary = it.summary, keywords = it.keywords, media = it.media.map { Media(category = it.category, url = it.url) }) },
-                    social = updatedCV.social.map { Social(label = it.label,network = it.network,user = it.user,url = it.url) },
-                    employment = Employment(summary = updatedCV.employment.summary, history = updatedCV.employment.history.map { History(employer = it.employer, url = it.url,
-                            position = it.position, summary = it.summary, start = it.start, end = it.end, keywords = it.keywords, highlights = it.highlights)}),
+                    projects = updatedCV.projects.map {
+                        Project(title = it.title, category = it.category, role = it.role, url = it.url, start = it.start, end = it.end,
+                                repo = it.repo, description = it.description, summary = it.summary, keywords = it.keywords, media = it.media.map { Media(category = it.category, url = it.url) })
+                    },
+                    social = updatedCV.social.map { Social(label = it.label, network = it.network, user = it.user, url = it.url) },
+                    employment = Employment(summary = updatedCV.employment.summary, history = updatedCV.employment.history.map {
+                        History(employer = it.employer, url = it.url,
+                                position = it.position, summary = it.summary, start = it.start, end = it.end, keywords = it.keywords, highlights = it.highlights)
+                    }),
                     education = Education(summary = updatedCV.education.summary, level = updatedCV.education.level, degree = updatedCV.education.degree,
-                            history = updatedCV.education.history.map { DegreeHistory(institution = it.institution, title = it.title, url = it.url, start = it.start,
-                                    end = it.end, grade = it.grade, summary = it.summary, curriculum = it.curriculum) }),
+                            history = updatedCV.education.history.map {
+                                DegreeHistory(institution = it.institution, title = it.title, url = it.url, start = it.start,
+                                        end = it.end, grade = it.grade, summary = it.summary, curriculum = it.curriculum)
+                            }),
                     skills = Skills(sets = updatedCV.skills.sets.map { org.mmkulmala.cvbank.data.Set(name = it.name, level = it.level, skills = it.skills) },
                             list = updatedCV.skills.list.map { Skill(name = it.name, summary = it.summary, level = it.level, years = it.years, proof = it.proof) }),
-                    samples = updatedCV.samples.map { Sample(title = it.title,summary = it.summary,url = it.url,date = it.date) },
+                    samples = updatedCV.samples.map { Sample(title = it.title, summary = it.summary, url = it.url, date = it.date) },
                     references = updatedCV.references.map { Reference(name = it.name, flavor = it.flavor, private = it.private, contact = it.contact.map { ReferenceContact(label = it.label, flavor = it.flavor, value = it.value) }) },
                     languages = updatedCV.languages.map { Language(language = it.language, level = it.level) },
                     interests = updatedCV.interests.map { Interest(name = it.name, summary = it.summary, keywords = it.keywords) }))
+
 }
